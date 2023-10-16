a67bfba11df19beb9116867ccdbe674e52a02c61
Andrea Riley <andreariley@me.com>
2023-10-16 18:18:34 -0400

    lab 8 sumbit

--- script.js
+++ script.js
@@ -3,12 +3,22 @@
 console.log('This is my log');
 alert('There is valuable information here')
 
+function NameIs(){
 const usersName = prompt('Who are you?')
-console.log('usersName');
 
-document.write('Hey ' + usersName + ' Here is info you did not know you needed until now')
+return usersName;
+}
 
-const animal = prompt('Do you like cats?')
+let HiIAm = NameIs();
+
+console.log('HiIAm');
+
+function greeting(){
+    document.write('Hey ' + HiIAm + ' Here is info you didnt know you needed until now');
+}
+
+function catLoverOrNot() {
+    const animal = prompt('Do you like cats?')
  console.log('animal');
 
 if (animal == "yes"){
@@ -18,4 +28,5 @@ else if (animal== "no"){
          document.write(' Welcome Dog Drooler.');
 } else {
     document.write(' You made it!');
+}
 }
