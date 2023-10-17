3810c7a6afcc6a17f42a906ba354840d8d705462
Andrea Riley <andreariley@me.com>
2023-10-17 15:22:53 -0400

    final lab

--- script.js
+++ script.js
@@ -19,14 +19,32 @@ function greeting(){
 
 function catLoverOrNot() {
     const animal = prompt('Do you like cats?')
- console.log('animal');
+ console.log(animal);
 
-if (animal == "yes"){
+
+    if (animal == "yes") {
      document.write(' Welcome Cat Lover!');
-} 
-else if (animal== "no"){
-         document.write(' Welcome Dog Drooler.');
-} else {
+}   else if (animal == "no") {
+    document.write(' Welcome Dog Drooler.');
+}    else {
     document.write(' You made it!');
 }
-}
+}       
+        
+function rateMyPage() {
+    let rating = prompt('Give my page a rating 1-10');
+    
+    while (true) {
+        if (rating >= 1 && rating <= 10) {
+            console.log(rating);
+        
+            for (let c = 0; c < rating; c++){
+                document.write("<img src='kitten.jpeg' alt='orange tabby kitten' id='kitten-photo' />");
+
+            }
+            break;
+        }else {
+            rating = prompt('Please try again.')
+        }
+    }
+  }
